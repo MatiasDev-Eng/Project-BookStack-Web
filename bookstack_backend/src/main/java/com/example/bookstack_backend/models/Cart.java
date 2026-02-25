101a773d43c638e5df576b624f3a887addbc03bb
leo <lgiacobbe14@gmail.com>
2026-02-25 12:35:04 -0600

    Complete more models

---
+++ bookstack_backend/src/main/java/com/example/bookstack_backend/models/Cart.java
@@ -0,0 +1,38 @@
+package com.example.bookstack_backend.models;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // The list of items inside this cart
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    public Long getCartId() { return cartId; }
+    public void setCartId(Long cartId) { this.cartId = cartId; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public List<CartItem> getItems() { return items; }
+    public void setItems(List<CartItem> items) { this.items = items; }
+}
---
+++ bookstack_backend/src/main/java/com/example/bookstack_backend/models/OrderItem.java
@@ -0,0 +1,52 @@
+package com.example.bookstack_backend.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    private BigDecimal priceAtPurchase;
+    private Integer quantity;
+
+    public OrderItem(Order order, Book book, BigDecimal priceAtPurchase, Integer quantity) {
+        this.order = order;
+        this.book = book;
+        this.priceAtPurchase = priceAtPurchase;
+        this.quantity = quantity;
+    }
+
+    public OrderItem() {
+
+    }
+
+    public Long getOrderItemId() { return orderItemId; }
+    public void setOrderItemId(Long orderItemId) { this.orderItemId = orderItemId; }
+
+    public Order getOrder() { return order; }
+    public void setOrder(Order order) { this.order = order; }
+
+    public Book getBook() { return book; }
+    public void setBook(Book book) { this.book = book; }
+
+    public BigDecimal getPriceAtPurchase() { return priceAtPurchase; }
+    public void setPriceAtPurchase(BigDecimal priceAtPurchase) { this.priceAtPurchase = priceAtPurchase; }
+
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+}
