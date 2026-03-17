559cae65cbe1ab644a19790205817578df948efa
Cara Belew <caragbelew@gmail.com>
2026-03-17 15:28:01 -0500

    Edited AngularCrud Test

--- bookstack_backend/src/test/java/com/example/bookstack_backend/AngularCrudInventoryApplicationTests.java
+++ bookstack_backend/src/test/java/com/example/bookstack_backend/AngularCrudInventoryApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.bookstack_backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AngularCrudInventoryApplicationTests {
 
     @Test
