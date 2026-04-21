1ec3c32078d3381df7fd78c9eae3a61e8442babd
Cara Belew <caragbelew@gmail.com>
2026-04-20 21:12:20 -0500

    admin pages check user role before loading page

--- bookstack_backend/src/main/java/com/example/bookstack_backend/controller/ErrorController.java
+++
@@ -1,8 +0,0 @@
-@Controller
-public class CustomErrorController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError() {
-        return "forward:/404.html";
-    }
-}
