5b701ae79b4acadd044a4c643bb814a301ac177a
Cara Belew <caragbelew@gmail.com>
2026-04-29 15:03:04 -0500

    Added Reject option for book and user approval + more books in view on homepage

--- bookstack_backend/src/main/java/com/example/bookstack_backend/repository/OrderRepository.java
+++ bookstack_backend/src/main/java/com/example/bookstack_backend/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.example.bookstack_backend.repository;
 
-
 import com.example.bookstack_backend.models.Order;
 import com.example.bookstack_backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,8 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserOrder(User userOrder);
+
+    @Modifying
+    @Query("UPDATE Order o SET o.creditCard = null WHERE o.creditCard.cardId = :cardId")
+    void nullifyCardReferences(@Param("cardId") Long cardId);
 }
